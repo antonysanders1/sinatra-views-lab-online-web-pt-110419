350cc751bc315adf0e9116546f8329ea6c9f6f78
Antony Sanders <antony.devon92@gmail.com>
2020-02-19 00:11:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do
+		erb :hello
+	end	
 
+<<<<<<< HEAD
 	get '/hello' do
 		erb :hello
 	end
@@ -16,5 +21,14 @@ class App < Sinatra::Base
 		erb :date
 	end
 
+=======
+	get '/goodbye' do
+		erb :goodbye
+	end
+>>>>>>> f785a045c1cff40d488cda9a7c2c67d33ffab504
 
+	get '/date' do
+		erb :date
+	end
+	
 end
